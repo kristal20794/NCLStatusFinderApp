14a3a238bfd1bfbb57b38eb0a60d8b7835eecdc4
kristal20794 <kristal20794@gmail.com>
2023-10-19 11:47:55 +0530

    Hackathon day 1  commit for NCLStatusFinder app

--- src/main/java/com/ncl/decom/apps/entity/ServiceStore.java
+++ src/main/java/com/ncl/decom/apps/entity/ServiceStore.java
@@ -1,15 +1,24 @@
 package com.ncl.decom.apps.entity;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 @Entity
+@Table(name="SERVERSTORE")
 public class ServiceStore {
 
-
+    @Id
+    @Column(name="INFRAID")
     private Long infraId;
+    @Column(name="SERVER_NAME")
+    private String serverName;
+    @Column(name="SERVER_TYPE")
+    private String serverType;
 
 }
